415c026ca947935c6bbfd8b51253bba7956dc422
israahamed765 <130075993+israahamed765@users.noreply.github.com>
2026-05-02 14:56:57 +0300

    Create TranslatorPage component
    Added TranslatorPage component with Arabic content.

--- src/app/translator/page.jsx
+++ src/app/translator/page.jsx
@@ -1,3 +1,15 @@
+
+function TranslatorPage() {
+  return (
+    <div>
+      {/* محتوى الصفحة هنا */}
+      <h1>صفحة التدريب</h1>
+    </div>
+  );
+}
+
+// السطر التالي هو الأهم والذي يسبب المشكلة إذا كان مفقوداً
+export default TranslatorPage;
 //  "use client";
 // import dynamic from "next/dynamic";
 
